050417d9991208f2f00fd4a4fba28b9870cb9439
suising <suising@waterstar.sg>
2015-09-25 23:27:40 +0000

    add unique_tag method test case

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,12 @@
 class Quote < ActiveRecord::Base
 	validates :saying, :presence => true, :length => { :maximum => 140, :minimum => 3 }
 	validates :author, :presence => true, :length => { :maximum => 50, :minimum => 3 }
+
+	def unique_tag
+		tag = self.author.split(" ").collect do | sub_str |
+			sub_str[0]
+		end
+
+		tag.join + '#' + self.id.to_s
+	end
 end
