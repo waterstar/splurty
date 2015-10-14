c912c04860e8f893361168f50fc8c627cec0b90d
suising <suising@waterstar.sg>
2015-10-14 18:25:26 +0000

    add test cases

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -11,4 +11,14 @@ class QuotesControllerTest < ActionController::TestCase
     get :show, :id => 'xyz'
     assert_response :not_found
   end
+
+  test "quote add new entry" do
+  	post :create, :quote => { :author => 'Sui Sing', :saying => 'This is a test quote.'}
+  	assert_redirected_to root_path
+  end
+
+  test "quote add new entry, validation fail" do
+  	post :create, :quote => { :author => '', :saying => 'This is a test quote.'}
+  	assert_response(:redirect)
+  end
 end
